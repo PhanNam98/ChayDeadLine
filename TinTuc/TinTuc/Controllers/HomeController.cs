bfb7553ff95bf8440615f2cf69abc8a7a1e23611
PhanNam98 <43126684+PhanNam98@users.noreply.github.com>
2019-04-16 00:52:09 +0700

    dead

---
+++ TinTuc/TinTuc/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TinTuc.Models;
+
+namespace TinTuc.Controllers
+{
+    public class HomeController : Controller
+    {
+        TinTucEntities db = new TinTucEntities();
+        public ActionResult Index()
+        {
+            var list = db.News.Take(3).ToList();
+            return View(list);
+        }
+
+        public ActionResult About(int id)
+        {
+            ViewBag.Message = "Chi tiết";
+            News a = db.News.Where(p => p.news_id == id).SingleOrDefault();
+            
+            return View(a);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
